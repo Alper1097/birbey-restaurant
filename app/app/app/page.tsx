5b5be0a4dbb29a167cf37769e74c6f3293c5d99d
Alper1097 <alperonal445@gmail.com>
2026-08-08 03:37:32 +0300

    Update page.tsx

--- app/app/app/page.tsx
+++ app/app/app/page.tsx
@@ -1,11 +1,17 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Services from "@/components/Services";
+import Catering from "@/components/Catering";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <About />
+      <Services />
+      <Catering />
     </main>
   );
 }
